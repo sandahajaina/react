24277d34b28970c1dad3f43ac24c4b174ae666b0
Sanda Hajaina Ranaivoarisoa <hajainasanda@gmail.com>
2026-07-01 19:01:52 +0300

    useEffect done

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
 import './App.css'
 import UserCard from './components/UserCard';
-import { UserCardProps } from './components/UserCard';
-import { useEffect, useState } from 'react';
+import type { UserCardProps } from './components/UserCard';
+import { use, useEffect, useState } from 'react';
+
+interface AppUser extends UserCardProps {
+    id: number;
+}
 
 interface ApiUser {
     id: number;
@@ -14,31 +18,45 @@ interface ApiUser {
 
 function App() {
 
-  const [users, setUsers] = useState<UserCardProps[]>([]);
+  const [users, setUsers] = useState<AppUser[]>([]);
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     async function fetchUsers() {
+
+      try {
         const response = await fetch('https://jsonplaceholder.typicode.com/users');
+
+        if (!response.ok) {
+          throw new Error(`Erreur HTTP : ${response.status}`);          
+        }
+
         const data: ApiUser[] = await response.json();
 
-        const formattedUsers: UserCardProps[] = data.map(user => {
-            // ⬅️ À toi de construire l'objet UserCardProps ici
-            // username: vient de user.username
-            // age: invente une valeur (ex: Math.floor(Math.random() * 30) + 18)
-            // city: vient de user.address.city
-            // online: invente une valeur (ex: Math.random() > 0.5)
-        });
+        const formattedUsers: AppUser[] = data.map(user => ({
+              id : user.id,
+              username: user.username,
+              age: Math.floor(Math.random() * 30) + 18,
+              city: user.address.city,
+              online : Math.random() > 0.5
+        }));
 
         setUsers(formattedUsers);
+      } catch (error) {
+        console.error('Erreur lors de la récupération des utilisateurs :', error);
+      } finally {
+        setIsLoading(false);
+      }
     }
     fetchUsers();
 }, []);
 
   return (
     <div>
-        <UserCard username='coco_loco' age={22} city="Paris" online={true}/>
-        <UserCard username='aya' age={25} city="Tana" online={false}/>
-        <UserCard username='bigga' age={32} city="Yaounde" online={true}/>
+        {isLoading
+          ? <p>Chargement des profils...</p>
+          : users.map(user => <UserCard key={user.id} {...user} />)
+        }
     </div>
   )
 }

77a6df45631489fcb4652f572f05be3257b6ed55
Sanda Hajaina Ranaivoarisoa <hajainasanda@gmail.com>
2026-07-22 17:48:04 +0300

    loginPage stuff

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,6 +1,40 @@
+import { Link } from "react-router-dom";
+import React, { useState } from "react";
+// import { Link, useNavigate } from "react-router-dom";
+
 function LoginPage() {
+
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [isLoading, setIsLoading] = useState(false);
+
+    function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
+        e.preventDefault();
+
+        setIsLoading(true);
+        console.log({email, password});
+    }
+
     return (
-        <h1>Login page</h1>
+        <div>
+            <h1>Login page</h1>
+            <form onSubmit={handleSubmit}>
+                <input
+                    type="email"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                    placeholder="email"
+                />
+                <input
+                    type="password"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                    placeholder="password"
+                />
+                <button type="submit" disabled={isLoading}>Se connecter</button>
+            </form>
+            <Link to='/register'>Register</Link>
+        </div>
     )
 }
 
